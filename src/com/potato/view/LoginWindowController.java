88f171a400321be67862415690b01886f3ceafdd
suabicii <michal.slabik3@wp.pl>
2021-04-26 16:16:43 +0200

    Builded login window

---
+++ src/com/potato/view/LoginWindowController.java
@@ -0,0 +1,24 @@
+package com.potato.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("Clicked!");
+    }
+
+}
