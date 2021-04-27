c37c0b785cbdef622e61c1537e59e8275ceb9f4a
suabicii <michal.slabik3@wp.pl>
2021-04-27 13:04:45 +0200

    Initialized stages

--- src/com/potato/view/ViewFactory.java
+++ src/com/potato/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package com.potato.view;
 
 import com.potato.EmailManager;
+import com.potato.controller.BaseController;
+import com.potato.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
 
@@ -11,6 +19,20 @@ public class ViewFactory {
     }
 
     public void showLoginWindow() {
-        System.out.println("Show login window called");
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 }
