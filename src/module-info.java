6b0787094bccbde46ab428f36fa9400dd9f35b8d
suabicii <michal.slabik3@wp.pl>
2021-04-28 16:09:09 +0200

    Added libraries: JavaMail and Activation

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,8 @@ module JavaFXEmailClient {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.web;
+    requires activation;
+    requires jakarta.mail;
 
     opens com.potato;
     opens com.potato.controller;
