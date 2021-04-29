4d8ed9bde8acfdce67e2b91a1438f9fec700c309
suabicii <michal.slabik3@wp.pl>
2021-04-29 12:17:00 +0200

    Extended loginButtonAction method

--- src/com/potato/controller/LoginWindowController.java
+++ src/com/potato/controller/LoginWindowController.java
@@ -5,12 +5,16 @@ import com.potato.controller.services.LoginService;
 import com.potato.model.EmailAccount;
 import com.potato.view.ViewFactory;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LoginWindowController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginWindowController extends BaseController implements Initializable {
 
     @FXML
     private TextField emailAddressField;
@@ -37,6 +41,17 @@ public class LoginWindowController extends BaseController {
                 switch (emailLoginResult) {
                     case SUCCESS:
                         System.out.println("login successful!!!" + emailAccount);
+                        viewFactory.showMainWindow();
+                        Stage stage = (Stage) errorLabel.getScene().getWindow();
+                        viewFactory.closeStage(stage);
+                        return;
+                    case FAILED_BY_CREDENTIALS:
+                        errorLabel.setText("invalid credentials");
+                        return;
+                    case FAILED_BY_UNEXPECTED_ERROR:
+                        errorLabel.setText("unexpected error");
+                        return;
+                    default:
                         return;
                 }
             });
@@ -58,5 +73,11 @@ public class LoginWindowController extends BaseController {
         }
 
         return true;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        emailAddressField.setText("java.course.kovalsky@gmail.com");
+        passwordField.setText("Genesis2k21!");
     }
 }
